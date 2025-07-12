07b3f012ece83f6eb8290ef3b02ee6fc5ea04d43
Sumant-work <sumantraj.work@gmail.com>
2025-07-12 11:58:22 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sanitary Inspector App',
+      theme: ThemeData(primarySwatch: Colors.teal),
       home: Scaffold(
-        appBar: AppBar(title: Text('Welcome')),
-        body: Center(child: Text('Hello Sanitary Inspector!')),
+        appBar: AppBar(title: Text('Sanitary Inspector App')),
+        body: Center(child: Text('Welcome to the Sanitary Inspector App')),
       ),
     );
   }
